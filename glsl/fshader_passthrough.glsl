8a0f9df9cbc471c37ea0838390be41ee7ba8198f
Mingching <mingchingchu@gmail.com>
2016-05-02 11:41:52 -0400

    blinn-phong in fshader

--- glsl/fshader_passthrough.glsl
+++ glsl/fshader_passthrough.glsl
@@ -2,13 +2,43 @@
 // actually interpolated from values specified in the vertex shader - and
 // just pass them through to the render:
 // on mac: 
+//varying vec4 norm;
 varying vec4 norm;
-//in vec4 norm;
+varying vec4 v_light;
+varying vec4 v_viewer;
+varying vec4 vnorm;
+
+uniform vec4 light_pos;
+uniform vec4 light_ambi;
+uniform vec4 light_diff;
+uniform vec4 light_spec;
+
+uniform vec4 material_ambi;
+uniform vec4 material_diff;
+uniform vec4 material_spec;
+uniform float material_shin;
 
 void main() 
-{ 
-  // "gl_FragColor" is already defined for us - it's the one thing you have
-  // to set in the fragment shader:
-  gl_FragColor = norm;
+{
+	// for ambient:
+	norm = light_ambi * material_ambi;
+	
+	// for diffuse:
+	float dd = max (0.0, dot(v_light, vnorm));
+	norm += dd * (light_diff * material_diff);
+	
+	// for specular:
+	float sd = 0.0;
+	if ((dot(v_light, vnorm) > 0.0) && (dot(v_viewer, vnorm) > 0.0)) {
+		sd = max(dot(normalize(v_light + v_viewer), vnorm), 0.0);
+	}
+	if (sd > 0.0) {
+		sd = pow(sd, material_shin);
+	}
+	norm += sd * (light_spec * material_spec);
+	
+	norm.a = 1.0;
+	
+	gl_FragColor = norm;
 } 
 
--- glsl/vshader_blinnphong.glsl
+++ glsl/vshader_blinnphong.glsl
@@ -1,48 +1,20 @@
 attribute vec4 vPosition;
 attribute vec4 vNorm;
+// in vec4 vPosition;
+// in vec4 vNorm;
 
 uniform mat4 ctm;
 uniform mat4 ptm;
-// camera position
-uniform vec4 view_pos;
-
-uniform vec4 light_pos;
-uniform vec4 light_ambi;
-uniform vec4 light_diff;
-uniform vec4 light_spec;
-
-uniform vec4 material_ambi;
-uniform vec4 material_diff;
-uniform vec4 material_spec;
-uniform float material_shin;
 
 // color, sned to fshader
 varying vec4 norm;
+varying vec4 v_light;
+varying vec4 v_viewer;
+varying vec4 vnorm;
 
 void main()
 {
-	// the direction from vertex to the light source and to the viewer
-	vec4 v_light = normalize(light_pos - vPosition);
-	vec4 v_viewer = normalize(view_pos - vPosition);
-	
-	// for ambient:
-	norm = light_ambi * material_ambi;
-	
-	// for diffuse:
-	float dd = max (0.0, dot(v_light, vNorm));
-	norm += dd * (light_diff * material_diff);
-
-	// for specular:
-	float sd = 0.0;
-	if ((dot(v_light, vNorm) > 0.0) && (dot(v_viewer, vNorm) > 0.0)) {
-		sd = max(dot(normalize(v_light + v_viewer), vNorm), 0.0);
-	}
-	if (sd > 0.0) {
-		sd = pow(sd, material_shin);
-	}
-	norm += sd * (light_spec * material_spec);
-	
-	norm.a = 1.0;
+	vnorm = vNorm;
 	
 	gl_Position = ptm * ctm * vPosition;
 }
